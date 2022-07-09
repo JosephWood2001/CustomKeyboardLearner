0112797f3f98de079338dcb94c281a3403a6846c
Joseph Wood <40001404+JosephWood2001@users.noreply.github.com>
2022-07-09 14:27:15 -0400

    add lesson buttons

---
+++ Lesson.py
@@ -0,0 +1,7 @@
+from random import random
+
+
+class Lesson():
+    
+    def __init__(self):
+        self.name = str(random())
--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+from Lesson import Lesson
+
 
 if __name__ == "__main__":
-   print("hello world")
+   root = Tk()
+   home = ttk.Frame(root, padding=10)
+   home.grid()
+   ttk.Label(home, text="Lessons").grid(column=0, row=0)
+
+   lessons:list[Lesson] = []
+   lessons.append(Lesson())
+   lessons.append(Lesson())
+   
+   for i, lesson in enumerate(lessons):
+      ttk.Button(home, text=lesson.name).grid(column=0,row=i+1)
+
+   root.mainloop()
