e2fad81ce947f5b80faf431284379831c4fe5323
Joseph Wood <40001404+JosephWood2001@users.noreply.github.com>
2022-07-10 12:58:25 -0400

    add colors for letters

--- Lesson.py
+++ Lesson.py
@@ -4,6 +4,10 @@ from tkinter import ttk
 
 import app
 
+rowLength = 30
+
+
+
 class Lesson():
     
     def __init__(self, name, speed, accuracy, showKeys, text):
@@ -14,12 +18,62 @@ class Lesson():
         self.text = text
 
     def init(self,root,home):
+        self.cursorIndex = 0
+
         self.frame = ttk.Frame(root, padding=10)
-        ttk.Label(self.frame, text=self.name).pack(pady=20)
-        ttk.Button(self.frame, text="Home", command=lambda: app.closeLesson(self,home)).pack()
+        ttk.Label(self.frame, text=self.name).grid(column=0,row=0)
+
+        self.textFrame = ttk.Frame(self.frame)
+        self.letters:list[ttk.Label] = []
+        self.correctLetters:list[int] = []
+        self.incorrectLetters:list[int] = []
+        self.fixedLetters:list[int] = []
+        i = 0
+        j = 0
+        for letter in self.text:
+            tmp = ttk.Label(self.textFrame, text=letter)
+            self.letters.append(tmp)
+            tmp.grid(column=j,row=i)
+            j+=1
+            if j >= rowLength and letter == ' ':
+                j=0
+                i+=1
+        self.textFrame.grid(column=0,row=1)
+        self.updateLetterColors()
+
+        ttk.Button(self.frame, text="Home", command=lambda: app.closeLesson(self,home)).grid(column=0,row=2)
 
         ttk.Button(home, text=self.name, command=lambda: app.openLesson(self,home)).pack()
 
+    def updateLetterColors(self):
+        
+        for i, letter in enumerate(self.letters):
+            if self.cursorIndex == i:
+                letter.configure(style="current.Label")
+                continue
+            
+            try:
+                self.correctLetters.index(i)
+                letter.configure(style="correct.Label")
+                continue
+            except ValueError:
+                pass
+
+            try:
+                self.incorrectLetters.index(i)
+                letter.configure(style="incorrect.Label")
+                continue
+            except ValueError:
+                pass
+
+            try:
+                self.fixedLetters.index(i)
+                letter.configure(style="fixed.Label")
+                continue
+            except ValueError:
+                pass
+            
+            letter.configure(style="blank.Label")
 
 
 def loadLesson(fileName:str,root,home) -> Lesson:
--- app.py
+++ app.py
@@ -2,19 +2,31 @@ from tkinter import *
 from tkinter import ttk
 from os import listdir
 from os.path import isfile, join
+import tkinter
+from tkinter.font import Font
 
 import Lesson
 
 def openLesson(lesson,home):
    home.forget()
-   lesson.frame.pack()
+   lesson.frame.grid()
 
 def closeLesson(lesson,home):
-   lesson.frame.forget()
-   home.pack()
+   lesson.frame.grid_forget()
+   home.pack(fill='both',expand=1)
 
 if __name__ == "__main__":
    root = Tk()
+
+   lettersFont = Font(size=20, family="Courier")
+   style = ttk.Style()
+   style.configure("current.Label", background='lightblue', font=lettersFont)
+   style.configure("correct.Label", background='lightgreen', font=lettersFont)
+   style.configure("incorrect.Label", background='red', font=lettersFont)
+   style.configure("fixed.Label", background='yellow', font=lettersFont)
+   style.configure("blank.Label", font=lettersFont)
+
+
    home = ttk.Frame(root, padding=10)
    ttk.Label(home, text="Lessons").pack(pady=20)
 
